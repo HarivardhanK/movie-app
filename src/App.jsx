a362dff58455e5469603085ad897d2e3be8cee60
HarivardhanK <kanthala.hari.198@gmail.com>
2024-04-20 21:49:55 +0530

    handling no movies with search query

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,46 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
 import Card from './Card';
-const App = ()=>{
-   
-    const [searchItem, setSearchItem] =useState("");
-    const [movies,setMovies] = useState([]);
-    const movieurl='https://www.omdbapi.com/?apikey=d308c665';
-    const searchMovie = async(title)=>{
-        const response = await fetch(`${movieurl}&s=${title}`);
-        const data = await response.json();
-        setMovies(data.Search);
-        console.log(data.Search);
-    }
-    useEffect(() => {
-        searchMovie('superman');
-    }, [])
-    
-    return (
-        <>
-        <section>
-            <h1> HV Movie Application</h1>   
-            <div className="container">
-            <input 
-                type='text' 
-                placeholder="search for your favourite movies"
-                value={searchItem}
-                onChange={(e)=>setSearchItem(e.target.value)}
-                />
-                <button
-                onClick={()=>searchMovie(searchItem)}
-                >SEARCH</button>
-            </div> 
-            <div className="card-container">
-                {movies.map((mov)=><Card movie={mov}/>)}
-            </div>
-        </section>
-        <footer>
-            Made with ❤ by Harivardhan
-        </footer>
+const App = () => {
 
-        </>
-    )
+	const [searchItem, setSearchItem] = useState("superman");
+	const [movies, setMovies] = useState([]);
+	const movieurl = 'https://www.omdbapi.com/?apikey=d308c665';
+
+	const searchMovie = async () => {
+		const response = await fetch(`${movieurl}&s=${searchItem}`);
+		const data = await response.json();
+		setMovies(data.Search);
+	}
+
+	useEffect(() => {
+		searchMovie();
+	}, [])
+
+	return (
+		<>
+			<section>
+				<h1> HV Movie Application</h1>
+				<div className="container">
+					<input
+						type='text'
+						placeholder="search for your favourite movies"
+						value={searchItem}
+						onChange={(e) => setSearchItem(e.target.value)}
+					/>
+					<button
+						onClick={() => searchMovie()}
+					>SEARCH</button>
+				</div>
+				<div className="card-container">
+					{(movies) ? movies.map((mov, index) => <Card movie={mov} key={index} />) : `No movies with ${searchItem} name. Please try with other movie name.`}
+				</div>
+			</section>
+			<footer>
+				Made with ❤ by Harivardhan
+			</footer>
+
+		</>
+	)
 }
 export default App;
